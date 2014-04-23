742c301766c630ce3b4a08789bf0cf27e7fc0164
Mark Hammond <mhammond@skippinet.com.au>
2014-04-23 15:01:06 +1000

    first cut at a loop-specific API and browser integration

---
+++ browser/base/content/browser-loop.js
@@ -0,0 +1,35 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+// the "exported" symbols
+let LoopUI;
+
+XPCOMUtils.defineLazyModuleGetter(this, "injectLoopAPI", "resource:///modules/loop/MozLoopAPI.jsm");
+
+
+(function() {
+
+  LoopUI = {
+    openCallPanel: function(event) {
+      let panel = document.getElementById("loop-panel");
+      let anchor = event.target;
+      let iframe = document.getElementById("loop-panel-frame");
+
+      // We inject in DOMContentLoaded as that is before any scripts have tun.
+      iframe.addEventListener("DOMContentLoaded", function documentDOMLoaded() {
+        iframe.removeEventListener("DOMContentLoaded", documentDOMLoaded, true);
+        injectLoopAPI(iframe.contentWindow);
+      }, true);
+
+      iframe.addEventListener("load", function documentLoaded() {
+        iframe.removeEventListener("load", documentLoaded, true);
+        sizeSocialPanelToContent(panel, iframe);
+      }, true);
+
+      iframe.setAttribute("src", "about:looppanel");
+      panel.hidden = false;
+      panel.openPopup(anchor, "bottomcenter topright", 0, 0, false, false);
+    },
+  }
+})();
